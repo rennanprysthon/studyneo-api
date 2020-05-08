38b9a9bc1b752aa4b22ed365c799c85a7613a061
Filipe Melo <filipemelo032@gmail.com>
2020-05-07 21:40:37 -0300

    modified user migration

--- backend/database/migrations/1503250034279_user.js
+++ backend/database/migrations/1503250034279_user.js
@@ -7,9 +7,10 @@ class UserSchema extends Schema {
   up () {
     this.create('users', (table) => {
       table.increments()
-      table.string('username', 80).notNullable().unique()
       table.string('email', 254).notNullable().unique()
       table.string('password', 60).notNullable()
+      table.string('name', 50).notNullable()
+      table.string('cpf', 11).notNullable().unique()
       table.timestamps()
     })
   }
