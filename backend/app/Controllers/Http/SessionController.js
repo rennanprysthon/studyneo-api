9266ad02a6822ac1ff0214881c36e18c427169ee
Filipe Melo <filipemelo032@gmail.com>
2020-05-09 14:33:38 -0300

    Refresh token middlware working

--- backend/app/Controllers/Http/SessionController.js
+++ backend/app/Controllers/Http/SessionController.js
@@ -3,7 +3,9 @@ class SessionController{
 
   async create({request, auth}){
     const {email, password} = request.post()
-    const token = await auth.attempt(email, password);
+    const token = await auth
+      .withRefreshToken()
+      .attempt(email, password);
 
     return token;
 
