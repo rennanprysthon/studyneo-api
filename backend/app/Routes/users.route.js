b43f0e194998734c41b59c5a1f4e729f6a7a5373
Filipe Melo <filipemelo032@gmail.com>
2020-05-10 21:52:54 -0300

    Authentication middlewares set

--- backend/app/Routes/users.route.js
+++ backend/app/Routes/users.route.js
@@ -3,9 +3,9 @@
 const Route = use('Route')
 
 Route.post('/users', 'UserController.create')
-Route.get('/users', 'UserController.showAll').middleware(['auth','refresh_token'])
-Route.get('/users/:id', 'UserController.show').middleware(['auth','refresh_token'])
-Route.put('/users', 'UserController.update').middleware(['auth','refresh_token'])
-Route.delete('/users', 'UserController.remove').middleware(['auth','refresh_token'])
+Route.get('/users', 'UserController.showAll').middleware('auth')
+Route.get('/users/:id', 'UserController.show').middleware('auth')
+Route.put('/users', 'UserController.update').middleware('auth')
+Route.delete('/users', 'UserController.remove').middleware('auth')
 
 module.exports = Route;
