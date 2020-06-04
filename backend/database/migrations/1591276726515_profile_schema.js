5e8a972f208df226f3a3e51367c0ba40b6a25562
Filipe Melo <filipemelo032@gmail.com>
2020-06-04 10:43:28 -0300

    profile entity added

---
+++ backend/database/migrations/1591276726515_profile_schema.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema');
+
+class ProfileSchema extends Schema {
+  up() {
+    this.create('profiles', (table) => {
+      table.increments();
+      table
+        .integer('user_id')
+        .unsigned()
+        .references('id')
+        .inTable('users')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+      table.enu('escolaridade', [
+        'Ensino Fundamental Incompleto',
+        'Ensino Fundamental Completo',
+        'Ensino Médio Incompleto',
+        'Ensino Médio Completo',
+        'Ensino Superior Incompleto',
+        'Ensino Superior Completo',
+      ]);
+      table.enu('rede', ['Pública', 'Privada']);
+      table.enu('area', ['Humanas', 'Linguagens', 'Exatas', 'Saúde']);
+      table.timestamps();
+    });
+  }
+
+  down() {
+    this.drop('profiles');
+  }
+}
+
+module.exports = ProfileSchema;
