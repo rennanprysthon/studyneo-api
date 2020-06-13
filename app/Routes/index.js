8cf04df03602837fea5d986744e4bd65ad37bab8
Rennan Prysthon <rennandelcastillo@gmail.com>
2020-06-12 22:22:53 -0300

    Update first route

--- app/Routes/index.js
+++ app/Routes/index.js
@@ -3,3 +3,15 @@
 require('./auth.route');
 require('./users.route');
 require('./profile.route');
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route');
+
+Route.get('/', () => {
+  return {
+    name: 'API para o aplicativo do Studyneo',
+    version: '0.0.1',
+  };
+});
+
+module.exports = Route;
