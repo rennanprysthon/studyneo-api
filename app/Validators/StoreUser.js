97f7389fe24186430d341aa53dc63efefaf2820f
Filipe Melo <filipemelo032@gmail.com>
2020-06-17 22:10:50 -0300

    sanitized User Validator

--- app/Validators/StoreUser.js
+++ app/Validators/StoreUser.js
@@ -1,27 +1,25 @@
-'use strict'
+'use strict';
 
 class StoreUser {
-  get rules () {
+  get rules() {
     return {
       email: 'required|email|unique:users',
       password: 'required',
-      cpf: 'unique:users'
-    }
+    };
   }
 
-  get messages () {
+  get messages() {
     return {
       'email.required': 'Você precisa informar o email de acesso.',
       'email.email': 'Você precisa informar um email válido.',
       'email.unique': 'O email informado já se encontra cadastrado.',
-      'cpf.unique': 'O CPF informado já se encontra cadastrado.',
-      'password.required': 'Você precisa informar uma senha.'
-    }
+      'password.required': 'Você precisa informar uma senha.',
+    };
   }
 
-  async fails (errorMessages) {
-    return this.ctx.response.send(errorMessages)
+  async fails(errorMessages) {
+    return this.ctx.response.send(errorMessages);
   }
 }
 
-module.exports = StoreUser
+module.exports = StoreUser;
