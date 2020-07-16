1fac17c5d10a85851382c9f6ba50939bfc56dc6e
Filipe Melo <filipemelo032@gmail.com>
2020-07-16 12:39:48 -0300

    admin middleware added

--- app/Routes/subject.route.js
+++ app/Routes/subject.route.js
@@ -1,6 +1,6 @@
 const Route = use('Route');
 
-Route.post('/subjects', 'SubjectController.create');
+Route.post('/subjects', 'SubjectController.create').middleware('admin');
 Route.get('/subjects', 'SubjectController.index');
 Route.get('/subjects/:id', 'SubjectController.show');
 Route.put('/subjects/:id', 'SubjectController.update').middleware('admin');
