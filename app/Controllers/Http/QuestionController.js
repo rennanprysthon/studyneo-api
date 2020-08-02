8a0cc69477fd800479bb6b5e50af745a0bed1dad
Filipe Melo <filipemelo032@gmail.com>
2020-08-02 11:56:33 -0300

    Merge branch 'develop'

--- app/Controllers/Http/QuestionController.js
+++ app/Controllers/Http/QuestionController.js
@@ -1,5 +1,7 @@
 'use strict';
 const Question = use('App/Models/Question');
+const Alternative = use('App/Models/Alternative');
+const Text = use('App/Models/Text');
 const Database = use('Database');
 const crypto = use('crypto');
 
@@ -61,16 +63,48 @@ class QuestionController {
     questions.total = Number(questions.total);
     return questions;
   }
-  // async update({ request }) {
-  //   const { id } = request.params;
-  //   const data = request.post();
+  async update({ request, response }) {
+    const { id } = request.params;
+    const { texts = [], alternatives, ...questionData } = request.post();
+    const question = await Question.find(id);
+    if (!question)
+      return response
+        .status(400)
+        .json({ message: 'Could not update such question' });
 
-  //   const question = await Question.find(Number(id));
-  //   question.merge(data);
-  //   await question.save();
+    question.merge(questionData);
+    await question.save();
 
-  //   return question;
-  // }
+    const updatedAlternatives = [];
+    for (let alternative of alternatives) {
+      const auxAlternative = await Alternative.find(alternative.id);
+      auxAlternative.merge(alternative);
+      await auxAlternative.save();
+      updatedAlternatives.push(auxAlternative);
+    }
+    question.alternatives = updatedAlternatives;
+
+    if (texts.length > 0) {
+      const updatedTexts = [];
+      for (let text of texts) {
+        let auxText;
+        if (text.id) {
+          auxText = await Text.find(text.id);
+          auxText.merge(text);
+          await auxText.save();
+        } else {
+          auxText = await question.texts().create(text);
+        }
+
+        updatedTexts.push(auxText);
+      }
+      question.texts = updatedTexts;
+    } else {
+      await question.texts().where('question_id', id).delete();
+    }
+
+    return question;
+  }
 
   async destroy({ request }) {
     const { id } = request.params;
