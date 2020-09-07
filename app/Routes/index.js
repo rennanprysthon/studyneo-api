c096820b0c47881d4aec6855686d0285d6eadf6d
Rennan Prysthon <rennandelcastillo@gmail.com>
2020-09-07 18:43:17 -0300

    Adicionando titulo em resumos

--- app/Routes/index.js
+++ app/Routes/index.js
@@ -10,6 +10,7 @@ require('./matter.route');
 require('./question.route');
 require('./overview.route');
 require('./text.route');
+require('./feed.route');
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route');
@@ -17,10 +18,8 @@ const Route = use('Route');
 Route.get('/', () => {
   return {
     name: 'API para o aplicativo do Studyneo',
-    version: '0.0',
+    version: '0.1',
   };
 });
 
 module.exports = Route;
-
-require('./admin.route');
